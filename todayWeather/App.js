9ad3ebd4f9729518a547694efcebf30c7e40db4e
skynolimit <sky221b9@gmail.com>
2023-04-27 15:47:46 +0900

    featStyles

--- todayWeather/App.js
+++ todayWeather/App.js
@@ -4,8 +4,15 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={{ fontSize: 28 }}>Hi!</Text>
-      <StatusBar style="light" />
+      <View style={styles.city}>
+        <Text style={styles.cityName}>SEOUL</Text>
+      </View>
+      <View style={styles.weather}>
+        <View>
+          <Text>27</Text>
+          <Text>sunny</Text>
+        </View>
+      </View>
     </View>
   );
 }
@@ -13,11 +20,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: "orange",
   },
-  text: {
-    fontSize: 28,
+  city: {
+    flex: 1.3,
+    backgroundColor: "blue",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  cityName: {
+    color: "white",
+    fontSize: 60,
+    fontWeight: 600,
+  },
+  weather: {
+    flex: 3,
+    backgroundColor: "teal",
   },
 });
