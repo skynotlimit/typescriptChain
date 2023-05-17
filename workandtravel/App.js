1b0e1a98e203e98fafe470cc41ae830a58c64257
skynolimit <sky221b9@gmail.com>
2023-05-17 23:14:41 +0900

    featDeleteBtn

--- workandtravel/App.js
+++ workandtravel/App.js
@@ -1,5 +1,7 @@
 import { StatusBar } from "expo-status-bar";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { MaterialIcons } from "@expo/vector-icons";
+
 import {
   ScrollView,
   StyleSheet,
@@ -7,9 +9,11 @@ import {
   TouchableOpacity,
   TextInput,
   View,
+  Alert,
 } from "react-native";
 import { theme } from "./colors";
-import { Touchable } from "react-native";
+const storage_key = "@toDos";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 export default function App() {
   //현재 탭의 상태
   const [work, setWork] = useState(true);
@@ -29,14 +33,48 @@ export default function App() {
   const onChangeText = (e) => {
     setText(e);
   };
-  const addTodo = () => {
+  const addTodo = async () => {
     if (text === "") {
       return;
     }
+    const newTodo = { ...todo, [Date.now()]: { text, work } };
     setText("");
     setTodo(newTodo);
+    await saveTodos(newTodo);
   };
-  const newTodo = { ...todo, [Date.now()]: { text, work } };
+
+  const saveTodos = async (toSave) => {
+    const str = JSON.stringify(toSave);
+    //작성한 리스트를 저장(앱을 다시 켜도 남아있도록)
+    await AsyncStorage.setItem(storage_key, str);
+  };
+  const loadTodos = async () => {
+    try {
+      const s = await AsyncStorage.getItem(storage_key);
+      setTodo(JSON.parse(s));
+    } catch (e) {
+      alert("불러오기 실패");
+    }
+  };
+  const deleteTodo = (key) => {
+    Alert.alert("Delete ToDo", "are u sure?", [
+      { text: "CANCEL" },
+      {
+        test: "OK",
+        onPress: () => {
+          //얕은 복사로 같은 내용의 새로운 복사본을 만들고 거기서 특정 요소만 delete한다.
+          const newTodo = { ...todo };
+          delete newTodo[key];
+          //state가 수정되고 나서 set함수로 변경된다.
+          setTodo(newTodo);
+          saveTodos(newTodo);
+        },
+      },
+    ]);
+  };
+  useEffect(() => {
+    loadTodos();
+  }, []);
 
   return (
     <View style={styles.container}>
@@ -63,11 +101,16 @@ export default function App() {
           placeholder={work ? "Add a To do" : "Add Play"}
         />
         <ScrollView>
-          {Object.keys(todo).map((key) => (
-            <View style={styles.todo} key={key}>
-              <Text style={styles.todoText}>{todo[key].text}</Text>
-            </View>
-          ))}
+          {Object.keys(todo).map((key) =>
+            todo[key].work === work ? (
+              <View style={styles.todo} key={key}>
+                <Text style={styles.todoText}>{todo[key].text}</Text>
+                <TouchableOpacity onPress={() => deleteTodo(key)}>
+                  <MaterialIcons name="cancel" size={24} color="orange" />
+                </TouchableOpacity>
+              </View>
+            ) : null
+          )}
         </ScrollView>
       </View>
     </View>
@@ -98,6 +141,9 @@ const styles = StyleSheet.create({
     fontSize: 18,
   },
   todo: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
     marginTop: 10,
     marginHorizontal: 5,
     backgroundColor: "gray",
